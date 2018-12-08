7f580681ae07109dedaaa5b283e468ee091de18c
Kenny Oh <ohdoking@gmail.com>
2018-12-08 16:34:56 +0100

    Git init

---
+++ src/main/java/com/sesame/appointment/sesametestproject/SesameTestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.sesame.appointment.sesametestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesameTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesameTestProjectApplication.class, args);
+	}
+}
